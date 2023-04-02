449d781e595e28742493ec6f65aa937ae3420e52
Zanini <vitormarquesof@gmail.com>
2023-04-02 15:50:56 -0300

    Commit p2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import static java.net.http.HttpRequest.newBuilder;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -9,12 +11,12 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = newBuilder(endereco).GET().build();
@@ -26,38 +28,46 @@ public class App {
         List<Map<String, String>> listaDefilmes = parser.parse(body);
 
         // exibir e manipular os dados
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < listaDefilmes.size(); i++) {
             Map<String, String> filmes = listaDefilmes.get(i);
 
             String urlImagem = filmes.get("image");
             String titulo = filmes.get("title").replace(":", "-");
+            Double classificacao = Double.parseDouble(filmes.get("imDbRating"));
+
+            String textoFigurinha;
+            InputStream imagemZanini;
+            if (classificacao >= 8.0){
+                textoFigurinha = "TOPZERA";
+                imagemZanini = new FileInputStream(new File("sobreposicao/zanini-empolgado.jpeg"));
+            } else {
+                imagemZanini = new FileInputStream(new File("sobreposicao/zanini-desconfiado.jpeg"));
+                textoFigurinha = "NAHHH";
+            }
 
             InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = titulo +".png";
+            String nomeArquivo = titulo + ".png";
 
             var geradora = new GeradoraDeFigurinhas();
-            geradora.cria(inputStream, nomeArquivo);
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemZanini);
 
             System.out.println("\u001b[1mTítulo:\u001b[m" + filmes.get("fullTitle"));
             System.out.println();
-            Double classificacao = Double.parseDouble((filmes.get("imDbRating"))); 
-                for (int n = 1; n<= classificacao; n++) {
+            // Double classificacao = Double.parseDouble((filmes.get("imDbRating")));
+            for (int n = 1; n <= classificacao; n++) {
                 System.out.print("⭐");
             }
             System.out.println();
             System.out.println(escalaDeRating(Integer.parseInt(filmes.get("imDbRatingCount"))));
 
             System.out.println("\n");
-            }
-
-
-        
         }
-    
+
+    }
 
     public static String escalaDeRating(int n) {
         if (n >= 1000000) {
-            return  "🥇";
+            return "🥇";
         } else if (n >= 1000) {
             return "🥈";
         } else if (n >= 100) {
--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,20 +1,30 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.awt.font.TextLayout;
+import java.awt.BasicStroke;
 
 import javax.imageio.ImageIO;
+import javax.swing.plaf.basic.BasicArrowButton;
 
 public class GeradoraDeFigurinhas {
-    
 
-    void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+    void cria(InputStream inputStream, String nomeArquivo, String texto, InputStream inputStreamSobreposicao) throws Exception {
 
         // leitura da imagem
-        //InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
-        // InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+        // InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+        // InputStream inputStream = new
+        // URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
         //
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
@@ -27,29 +37,56 @@ public class GeradoraDeFigurinhas {
         // copiar a imagem original para nova imagem (em memória)
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
-        
-        //configurar a fonte
-        var fonte = new Font(java.awt.Font.SANS_SERIF, java.awt.Font.BOLD, 64);
+
+        BufferedImage imagemSobreposicao = ImageIO.read(inputStreamSobreposicao);
+        int posicaoImagemSobreposicaoY = novaAltura - imagemSobreposicao.getWidth(); 
+        graphics.drawImage(imagemSobreposicao, 0, posicaoImagemSobreposicaoY, null);
+
+        // configurar a fonte
+        var fonte = new Font("Impact", java.awt.Font.BOLD, 100);
         graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
 
-         // escrever uma frase em uma nova imagem
-        graphics.drawString("TOPZERA", 200, novaAltura - 100);
+        // escrever uma frase em uma nova imagem
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
+        retangulo.getWidth();
+        int larguraTexto = (int) retangulo.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+        int posicaoTextoY = novaAltura - 100;
+        graphics.drawString(texto, posicaoTextoX, posicaoTextoY);
 
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(texto, fonte, fontRenderContext);
+
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, posicaoTextoY);
+        graphics.setTransform(transform);
+
+
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
+
+        // criar uma diretorio caso nao exista
+        File file = new File("saida");
+        if (!file.exists()){
+        file.mkdir();
+        }
 
         // escrever a nova imagem em um arquivo
-        // File file = new File("saida");
-        // if (!file.exists()){
-        // file.mkdir();
-        // }
 
-
-        ImageIO.write(novaImagem, "png", new File("saida/" + nomeArquivo ));
+        ImageIO.write(novaImagem, "png", new File("saida/" + nomeArquivo));
     }
 
     // public static void main(String[] args) throws Exception {
-    //     var geradora = new GeradoraDeFigurinhas();
-    //     geradora.cria("abacate", "banana");
+    // var geradora = new GeradoraDeFigurinhas();
+    // geradora.cria("abacate", "banana");
     // }
 
 }
